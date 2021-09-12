16f1abe573549b38f2d754fede07bf00ad840400
Ricky <phangr28@gmail.com>
2021-09-12 20:59:01 +0700

    deploy vercel a bit

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+	useEffect(() => {
+		AOS.init({
+			offset: 100,
+			duration: 1000,
+			easing: "ease",
+		});
+	}, []);
+
 	return (
 		<ChakraProvider>
 			<Component {...pageProps} />
