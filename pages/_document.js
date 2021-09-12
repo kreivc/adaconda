91ac9aef3de050796a3ee2801f494710eeea4182
Ricky <phangr28@gmail.com>
2021-09-12 21:10:48 +0700

    fix theme

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-document-import-in-page */
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-import theme from "./theme";
+import theme from "../styles/theme";
 
 export default class Document extends NextDocument {
 	render() {
